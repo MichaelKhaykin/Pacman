45362252437d9b7adbb31c866be4ec65c1a76b0e
MoneyManMike <mr.khaykin@gmail.com>
2018-11-30 17:41:36 -0800

    Making map works need, new colors implemented which represent starting positions, and checks were made so they can't be used multiple times

--- PacMan/PacMan/MakeMapScreen.cs
+++ PacMan/PacMan/MakeMapScreen.cs
@@ -24,6 +24,7 @@ namespace PacMan
         bool checkForGrid = false;
         Button currButtonClicked = null;
 
+        Dictionary<Color, int> OneTimeUse;
 
         public MakeMapScreen(GraphicsDevice graphics, ContentManager content) : base(graphics, content)
         {
@@ -31,6 +32,7 @@ namespace PacMan
             pixel.SetData(new[] { Color.White });
 
             Buttons = new List<Button>();
+            OneTimeUse = new Dictionary<Color, int>();
 
             font = Content.Load<SpriteFont>("Font");
 
@@ -38,22 +40,42 @@ namespace PacMan
 
             Grid = new Sprite(gridTexture, new Vector2(500, 400), Color.White, Vector2.One * 16, null);
             //Grid.Origin = new Vector2(gridTexture.Width/2, gridTexture.Height/2);
-
-
+            
             var blank = Content.Load<Texture2D>("white1x1");
             
             var wallButton = new Button(blank, new Vector2(100, 50), Color.Black, Grid.Scale, null);
 
-            var invalidFoodButton = new Button(blank, new Vector2(100, 100), Color.Red, Grid.Scale, null);
+            var invalidFoodButton = new Button(blank, new Vector2(100, 100), Color.Green, Grid.Scale, null);
 
             var eraserButton = new Button(blank, new Vector2(100, 150), Color.White, Grid.Scale, null);
 
             var powerUpButton = new Button(blank, new Vector2(100, 200), Color.Gray, Grid.Scale, null);
-            
+
+            var pacmanButton = new Button(blank, new Vector2(100, 250), Color.Yellow, Grid.Scale, null);
+
+            var inkyButton = new Button(blank, new Vector2(100, 300), Color.Blue, Grid.Scale, null);
+
+            var blinkyButton = new Button(blank, new Vector2(100, 350), Color.Red, Grid.Scale, null);
+
+            var pinkyButton = new Button(blank, new Vector2(100, 400), Color.Pink, Grid.Scale, null);
+
+            var orangeButton = new Button(blank, new Vector2(100, 450), Color.Orange, Grid.Scale, null);
+
             Buttons.Add(wallButton);
             Buttons.Add(invalidFoodButton);
             Buttons.Add(eraserButton);
             Buttons.Add(powerUpButton);
+            Buttons.Add(pacmanButton);
+            Buttons.Add(inkyButton);
+            Buttons.Add(blinkyButton);
+            Buttons.Add(pinkyButton);
+            Buttons.Add(orangeButton);
+            
+            OneTimeUse.Add(Color.Yellow, 0);
+            OneTimeUse.Add(Color.Red, 0);
+            OneTimeUse.Add(Color.Blue, 0);
+            OneTimeUse.Add(Color.Orange, 0);
+            OneTimeUse.Add(Color.Pink, 0);
         }
         
         public (int, int) GetGridCellPosition()
@@ -74,7 +96,6 @@ namespace PacMan
 
         public override void Update(GameTime gameTime)
         {
-          
             foreach (var button in Buttons)
             {
                 if(button.IsClicked(Game1.Mouse) && !button.IsClicked(Game1.OldMouse))
@@ -90,6 +111,14 @@ namespace PacMan
             
             if(checkForGrid)
             {
+                if(OneTimeUse.ContainsKey(currButtonClicked.Color))
+                {
+                    if(OneTimeUse[currButtonClicked.Color] == 1)
+                    {
+                        return;
+                    }
+                }
+
                 if (Grid.HitBox.Contains(Game1.Mouse.Position) && Game1.Mouse.LeftButton == ButtonState.Pressed)
                 {
                     var point = GetGridCellPosition();
@@ -102,7 +131,17 @@ namespace PacMan
                         //convert from 2d to 1d
                         if (i % Grid.Texture.Width == point.Item1 && i / Grid.Texture.Width == point.Item2)
                         {
-                            colorarray[i] = (Color)currButtonClicked.Color;
+                            //if it is a onetimeuse color being changed to white
+                            //then we need to decrease the count in the dictionary
+                            if(OneTimeUse.ContainsKey(colorarray[i]) && currButtonClicked.Color == Color.White)
+                            {
+                                OneTimeUse[colorarray[i]]--;
+                            }
+                            colorarray[i] = currButtonClicked.Color;
+                            if(OneTimeUse.ContainsKey(currButtonClicked.Color))
+                            {
+                                OneTimeUse[currButtonClicked.Color]++;
+                            }
                         }
                     }
 
